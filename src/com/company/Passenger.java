6364a1367c81333ed828ef039925e6ea5dd68cd3
abdulashfaq <abdulashfaq.info@gmail.com>
2021-10-11 20:57:57 +0530

    written code for passenger class

--- src/com/company/Passenger.java
+++ src/com/company/Passenger.java
@@ -1,4 +1,53 @@
 package com.company;
 
 public class Passenger {
+    private static int idCounter;
+
+    static {
+        idCounter = 0;
+    }
+
+    private int id;
+    private Address address;
+    private Contact contact;
+
+    public Passenger(String addressStreet, String addressCity,
+                     String addressState, String contactName, String contactPhone,
+                     String contactEmail) {
+        this.id = ++idCounter;
+        this.address = new Address(addressStreet, addressCity, addressState);
+        this.contact = new Contact(contactName, contactPhone, contactEmail);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getAddressDetails() {
+        return address.street + ", " + address.city + ", " + address.state;
+    }
+
+    public String getContactDetails() {
+        return contact.name + ", " + contact.phone + ", " + contact.email;
+    }
+
+    private static class Address {
+        String street, city, state;
+
+        public Address(String street, String city, String state) {
+            this.street = street;
+            this.city = city;
+            this.state = state;
+        }
+    }
+
+    private static class Contact {
+        String name, phone, email;
+
+        public Contact(String name, String phone, String email) {
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
+        }
+    }
 }
